998267a885a586e46320e51860367dba1e896568
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-21 15:27:14 +0300

    bonus part update

--- bonus/donut.c
+++ bonus/donut.c
@@ -5,32 +5,35 @@
 #include <unistd.h>
 
 int main() {
-    float A = 0, B = 0;
-    float i, j;
-    int k;
+
+    float A = 0,B = 0, D, c, d, e, f, g, h, l, m, n, t;
+    int x,y,o, N;
+
     float z[1760];
     char b[1760];
+
     printf("\x1b[2J");
+
     for(;;) {
-        memset(b,32,1760);
-        memset(z,0,7040);
-        for(j=0; j < 6.28; j += 0.07) {
-            for(i=0; i < 6.28; i += 0.02) {
-                float c = sin(i);
-                float d = cos(j);
-                float e = sin(A);
-                float f = sin(j);
-                float g = cos(A);
-                float h = d + 2;
-                float D = 1 / (c * h * e + f * g + 5);
-                float l = cos(i);
-                float m = cos(B);
-                float n = sin(B);
-                float t = c * h * g - f * e;
-                int x = 40 + 30 * D * (l * h * m - t * n);
-                int y= 12 + 15 * D * (l * h * n + t * m);
-                int o = x + 80 * y;
-                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+        memset(b, 32, 1760);
+        memset(z, 0, 7040);
+        for(int j = 0; j < 6.28; j += 0.07) {
+            for(int i = 0; i < 6.28; i += 0.02) {
+                c = sin(i);
+                d = cos(j);
+                e = sin(A);
+                f = sin(j);
+                g = cos(A);
+                h = d + 2;
+                D = 1 / (c * h * e + f * g + 5);
+                l = cos(i);
+                m = cos(B);
+                n = sin(B);
+                t = c * h * g - f * e;
+                x = 40 + 30 * D * (l * h * m - t * n);
+                y= 12 + 15 * D * (l * h * n + t * m);
+                o = x + 80 * y;
+                N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
                 if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
                     z[o] = D;
                     b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
@@ -38,7 +41,7 @@ int main() {
             }
         }
         printf("\x1b[H");
-        for(k = 0; k < 1761; k++) {
+        for(int k = 0; k < 1761; k++) {
             putchar(k % 80 ? b[k] : 10);
             A += 0.00004;
             B += 0.00002;
